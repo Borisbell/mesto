2b44bc384aebdea0bb8b712d2d28b6a0872af338
Borisbell <jmorrison@bk.ru>
2022-03-31 16:15:44 +0400

    fix:popupwithimage updated

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -3,11 +3,10 @@ import { Popup } from './Popup.js';
 export class PopupWithImage extends Popup {
 
   openPopup = (name, link) => {
-    document.querySelector('.popup__description').textContent = name;
+    this._popup.querySelector('.popup__description').textContent = name;
     const zoomImage = document.querySelector('.popup__zoom-img');
     zoomImage.src = link;
     zoomImage.alt = name;
     super.openPopup();
-    document.addEventListener('keydown', this._handleEscClose);
   }
 }
