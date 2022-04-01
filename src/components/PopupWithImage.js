410e5ef78807c3cc204c80750753abd9d6bf818d
Borisbell <jmorrison@bk.ru>
2022-04-01 09:12:10 +0400

    fix popupwithimage replaced search in document

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -4,7 +4,7 @@ export class PopupWithImage extends Popup {
 
   openPopup = (name, link) => {
     this._popup.querySelector('.popup__description').textContent = name;
-    const zoomImage = document.querySelector('.popup__zoom-img');
+    const zoomImage = this._popup.querySelector('.popup__zoom-img');
     zoomImage.src = link;
     zoomImage.alt = name;
     super.openPopup();
